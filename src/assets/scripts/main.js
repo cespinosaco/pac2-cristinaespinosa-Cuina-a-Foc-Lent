877d119d4907de81362340172c98f86998b0ad95
Cristina Espinosa <cespinosaco@uoc.edu>
2025-12-07 20:07:31 +0100

    Projecte Cuina a Foc Lent

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,19 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  duration: 800,
+  easing: 'ease-out',
+  once: true,
+});
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+
